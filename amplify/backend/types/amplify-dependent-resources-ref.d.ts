47e4167c69e016e43f4b02943a1cde2c57d1c6b6
Jussi Vuorimäki <jussi.vuorimaki@valmetpartners.com>
2022-04-26 07:48:14 +0300

    Add storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "notesapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "s3001e36c9": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
